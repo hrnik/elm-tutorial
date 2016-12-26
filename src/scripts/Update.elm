451821f45fa4a4e917cc99dc662d1ad3e1478c4b
Nikita Khristinin <nkhristinin@gmail.com>
2016-12-26 20:28:26 +0200

    import Players module to Main

--- src/scripts/Update.elm
+++ src/scripts/Update.elm
@@ -2,10 +2,15 @@ module Update exposing (..)
 
 import Messages exposing (Msg(..))
 import Models exposing (Model)
+import Players.Update
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        NoOp ->
-            ( model, Cmd.none )
+        PlayersMsg subMsg ->
+            let
+                ( updatePlayers, cmd ) =
+                    Players.Update.update subMsg model.players
+            in
+                ( { model | players = updatePlayers }, Cmd.map PlayersMsg cmd )
